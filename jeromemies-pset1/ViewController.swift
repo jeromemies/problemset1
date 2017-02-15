913a2bed5dabb3768ad0c29eddb6fd4dab4c44e8
Jerome Mies <Jerome@jeromes-macbook-air.local>
2017-02-15 17:27:43 +0100

    final version

--- jeromemies-pset1/ViewController.swift
+++ jeromemies-pset1/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Test Label
     @IBOutlet weak var label: UILabel!
     
     @IBAction func `switch`(_ sender: UISwitch) {
@@ -19,10 +19,10 @@ class ViewController: UIViewController {
         else {
             label.text = "no"
         }
-        
-        
     }
+    // Use of switch according to https://www.youtube.com/watch?v=UDPJj3gnuZQ
     
+    // Ears
     @IBOutlet weak var ears: UIImageView!
     @IBAction func switchEars(_ sender: UISwitch) {
         if (sender.isOn == true){
@@ -31,11 +31,9 @@ class ViewController: UIViewController {
         else {
             ears.isHidden = true
         }
-        
-        
-    
     }
     
+    // Arms
     @IBOutlet weak var arms: UIImageView!
     @IBAction func switchArms(_ sender: UISwitch) {
         if (sender.isOn == true){
@@ -45,8 +43,87 @@ class ViewController: UIViewController {
             arms.isHidden = true
         }
     }
+    // Mouth
+    @IBOutlet weak var mouth: UIImageView!
+    @IBAction func switchMouth(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            mouth.isHidden = false
+        }
+        else {
+            mouth.isHidden = true
+        }
+    }
+    // Eyes
+    @IBOutlet weak var eyes: UIImageView!
+    @IBAction func switchEyes(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            eyes.isHidden = false
+        }
+        else {
+            eyes.isHidden = true
+        }
+    }
+    // Glasses
+    @IBOutlet weak var glasses: UIImageView!
+    @IBAction func switchGlasses(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            glasses.isHidden = false
+        }
+        else {
+            glasses.isHidden = true
+        }
+    }
+    // Hat
+    @IBOutlet weak var hat: UIImageView!
+    @IBAction func switchHat(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            hat.isHidden = false
+        }
+        else {
+            hat.isHidden = true
+        }
+    }
+    // Mustache
+    @IBOutlet weak var mustache: UIImageView!
+    @IBAction func switchMustache(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            mustache.isHidden = false
+        }
+        else {
+            mustache.isHidden = true
+        }
+    }
+    // Nose
+    @IBOutlet weak var nose: UIImageView!
+    @IBAction func switchNose(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            nose.isHidden = false
+        }
+        else {
+            nose.isHidden = true
+        }
+    }
     
+    //shoes
+    @IBOutlet weak var shoes: UIImageView!
+    @IBAction func switchShoes(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            shoes.isHidden = false
+        }
+        else {
+            shoes.isHidden = true
+        }
+    }
+    @IBOutlet weak var eyebrows: UIImageView!
     
+    @IBAction func switchEyebrows(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            eyebrows.isHidden = false
+        }
+        else {
+            eyebrows.isHidden = true
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
